0d57cebb9395ead2abc6b460dd463a957ae3e59a
Benjamin Cook <benjamincook@Benjamins-MacBook-Pro.local>
2018-06-08 21:30:51 -0400

    react-bootstrap form attempt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Button, ButtonToolbar, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -13,7 +13,21 @@ class App extends Component {
           Please Enter the Name of the Game Your Brought
         </p>
         <form>
-          <input type="text" name="name" />
+          <FormGroup
+            controlId="formBasicText"
+          // validationState={this.getValidationState()}
+          >
+            <ControlLabel>Working example with validation</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Enter text"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+            <HelpBlock>Validation is based on string length.</HelpBlock>
+          </FormGroup>
+          {/* <input type="text" name="name" /> */}
         </form>
         <ButtonToolbar>
           <Button bsStyle="primary" onClick={this.newTypeToDatabase}>Submit</Button>
