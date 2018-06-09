98d6a4491efd7ad7d7d2527ebe910ae43d492292
Benjamin Cook <benjamincook@benjamins-mbp.home>
2018-06-09 09:24:07 -0400

    made title appear

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Random Game Picker</h1>
+        <header className="App-header123" id="RandomGame">
+          <h1 className="App-title123" >Random Game Picker</h1>
         </header>
         <p className="App-intro">
           Please Enter the Name of the Game Your Brought
         </p>
         <form>
           <div className='form-group'>
-            <input type="text" className="form-control"
+            <input
+              type="text"
+              className="form-control"
               name="game" />
           </div>
         </form>
-        <ButtonToolbar>
-          <Button bsStyle="primary" onClick={this.newTypeToDatabase}>Submit</Button>
-        </ButtonToolbar>
+        <div className='button-for-submitting-games'>
+          <ButtonToolbar>
+            <Button bsStyle="primary" onClick={this.newTypeToDatabase}>Submit</Button>
+          </ButtonToolbar>
+        </div>
       </div >
     );
   }
