ec9301ef1122b1f7603899e81873dd6bdfce7eda
Benjamin Cook <benjamincook@benjamins-mbp.home>
2018-06-09 12:51:57 -0400

    wrote code to update state array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import './App.css';
 import { Button, ButtonToolbar, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      games: []
+    }
+  }
+
+  addToArray = () => {
+
+  }
+
+  updateName = (e) => {
+    this.setState({
+      games: [...this.state.games, e]
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -10,19 +28,32 @@ class App extends Component {
           <h1 className="App-title123" >Random Game Picker</h1>
         </header>
         <p className="App-intro">
-          Please Enter the Name of the Game Your Brought
+          Please Enter the Game's Name
         </p>
         <form>
           <div className='form-group'>
             <input
               type="text"
               className="form-control"
-              name="game" />
+              name="game"
+              placeholder="name"
+            />
+
           </div>
         </form>
         <div className='button-for-submitting-games'>
           <ButtonToolbar>
-            <Button bsStyle="primary" onClick={this.newTypeToDatabase}>Submit</Button>
+            <Button bsStyle="primary" onClick={this.addToArray}>Submit</Button>
+          </ButtonToolbar>
+        </div>
+        <div>
+          <p className="App-intro">
+            We're considering these games
+        </p>
+        </div>
+        <div className='button-for-random'>
+          <ButtonToolbar>
+            <Button bsStyle="primary" onClick={this.newTypeToDatabase}>Magic</Button>
           </ButtonToolbar>
         </div>
       </div >
