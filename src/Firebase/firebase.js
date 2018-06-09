385883af56948e55c324f595347cebb88ba29c61
Benjamin Cook <benjamincook@Benjamins-MacBook-Pro.local>
2018-06-08 20:30:56 -0400

    did this work

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,1 +1,14 @@
-// I no longer want this on Github
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyA3aCluPmXXUKg5rTWDYBP5XT-rKLQmq1I",
+    authDomain: "random-game-9d370.firebaseapp.com",
+    databaseURL: "https://random-game-9d370.firebaseio.com",
+    projectId: "random-game-9d370",
+    storageBucket: "random-game-9d370.appspot.com",
+    messagingSenderId: "1013494147540"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+} 
